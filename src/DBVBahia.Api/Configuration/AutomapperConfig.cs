5721ef4a92a0854dd4942bc671719875689018b7
Jodaias Barreto <jodaias2013@gmail.com>
2023-04-09 11:34:26 -0300

    Alterando para trazer o picture ao pegar todos os produtos

--- src/DBVBahia.Api/Configuration/AutomapperConfig.cs
+++ src/DBVBahia.Api/Configuration/AutomapperConfig.cs
@@ -29,12 +29,12 @@ namespace DBVBahia.Api.Configuration
 
             CreateMap<Endereco, EnderecoViewModel>().ReverseMap();
             CreateMap<Picture, PictureViewModel>()
-                .ForMember(dest => dest.Name, opt => opt.MapFrom(src=> src.Nome))
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Nome))
                 .ForMember(dest => dest.ImagemUpload, opt => opt.Ignore())
                 .ForMember(dest => dest.ImagemUpload64, opt => opt.Ignore())
                 .AfterMap((src, dest, context) =>
                 {
-                    if(src.Image != null && src.Image.Length > 0)
+                    if (src.Image != null && src.Image.Length > 0)
                     {
                         var imageBase64 = Convert.ToBase64String(src.Image);
                         dest.ImagemUpload64 = imageBase64;
@@ -54,18 +54,26 @@ namespace DBVBahia.Api.Configuration
 
             CreateMap<Produto, ProdutoViewModel>()
                 .ForPath(dest => dest.NomeFornecedor, opt => opt.MapFrom(src => src.Fornecedor.Nome))
+                .ForPath(dest => dest.Picture, opt => opt.Ignore())
+                .AfterMap((src, dest, context) =>
+                {
+                    if (src.Picture != null)
+                    {
+                        dest.Picture = context.Mapper.Map<PictureViewModel>(src.Picture);
+                    }
+                })
                 .ReverseMap()
-                .ForPath(dest=> dest.Fornecedor, opt=> opt.Ignore())
+                .ForPath(dest => dest.Fornecedor, opt => opt.Ignore())
                 .ForPath(dest => dest.Fornecedor.Nome, opt => opt.MapFrom(src => src.NomeFornecedor))
                 .AfterMap((src, dest, context) =>
                 {
-                   if (src.FornecedorId != Guid.Empty)
-                   {
+                    if (src.FornecedorId != Guid.Empty)
+                    {
                         dest.Fornecedor = new Fornecedor
                         {
                             Id = src.FornecedorId,
                         };
-                   }
+                    }
                 });
         }
     }
